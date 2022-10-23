5ee0afaa3812c4ccbc1fb81b2951289d4daa5759
Lewis <LYAKAKOY@yandex.ru>
2022-10-23 11:13:38 +0300

    LMS2.0

--- LMS/users/models.py
+++ LMS/users/models.py
@@ -1,3 +1,19 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserData(models.Model):
+    """Данные зарегестрированного пользователя"""
+
+    firstname = models.CharField('Имя пользователя', max_length=255, default='')
+    lastname = models.CharField('Фамилия пользователя', max_length=255, default='')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Данные пользователя'
+        verbose_name_plural = 'Данные пользователей'
--- LMS/users/views.py
+++ LMS/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import RegisterForm
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+
+class Register(CreateView):
+    form_class = RegisterForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('')
