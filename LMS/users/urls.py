037b3a5ff0b148dab0d29efd540c59ffc25b3ea5
Lewis <LYAKAKOY@yandex.ru>
2022-10-24 02:45:47 +0300

    Last version

--- LMS/users/urls.py
+++ LMS/users/urls.py
@@ -4,8 +4,8 @@ from .views import RegistrationUserView, AuntificationUserView, ChangePasswordUs
     ChangePasswordUserConfirm, ChangePasswordUserComplete
 
 urlpatterns = [
-    path('reg', RegistrationUserView.as_view(), name='reg'),
-    path('login', AuntificationUserView.as_view(), name='log'),
+    path('reg/', RegistrationUserView.as_view(), name='reg'),
+    path('login/', AuntificationUserView.as_view(), name='log'),
     path('pass-reset/', ChangePasswordUser.as_view(), name='pass-reset'),
     path('password_reset_confirm/<uidb64>/<token>/', ChangePasswordUserConfirm.as_view(),
          name='password_reset_confirm'),
